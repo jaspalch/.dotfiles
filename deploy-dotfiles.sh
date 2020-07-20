05f769b129de4e6dfd11118632ec02637b030180
Jaspal Chauhan <jaspalch95@gmail.com>
2020-07-20 02:20:35 -0700

    Switch to managing dotfiles using Ansible instead of yadm

---
+++ deploy-dotfiles.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+tags="${1}"
+[[ -z "${tags}" ]] && tags='all'
+
+if ! command -v ansible-playbook >& /dev/null; then
+    echo "Ansible is not installed, exiting..."
+    exit 1
+fi
+
+ansible-playbook -i ~/.dotfiles/hosts ~/.dotfiles/dotfiles.yml --tags "${tags}"
---
+++ roles/bash/files/work.bashrc
@@ -0,0 +1,2 @@
+export PYTHONIOENCODING=utf-8
+export PYTHONPATH=/home/tom/work/boto
