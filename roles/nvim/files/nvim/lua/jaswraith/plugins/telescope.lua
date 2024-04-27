999f2c868141773830d8039f447fd31f4418a6cd
Jaspal Chauhan <jaspalch95@gmail.com>
2024-04-27 00:24:52 -0700

    Goto last used buffer by default for telescope

--- roles/nvim/files/nvim/lua/jaswraith/plugins/telescope.lua
+++ roles/nvim/files/nvim/lua/jaswraith/plugins/telescope.lua
@@ -5,4 +5,13 @@ return {
         'nvim-lua/plenary.nvim',
         "nvim-tree/nvim-web-devicons",
     },
+    config = function()
+        require('telescope').setup({
+            pickers = {
+                buffers = {
+                    sort_lastused = true,
+                },
+            } 
+        })
+    end
 }
