1b8e94cd91b8c33bb3b2b9adc6c1f85ad953bb53
Jaspal Chauhan <jaspalch95@gmail.com>
2024-07-04 16:40:30 -0700

    Change colorscheme to nightfox

--- roles/nvim/files/nvim/lua/jaswraith/plugins/colorscheme.lua
+++ roles/nvim/files/nvim/lua/jaswraith/plugins/colorscheme.lua
@@ -1,12 +1,27 @@
+local palettes = {
+    nightfox = {
+        bg0 = "#0a111a",
+        bg1 = "#0a111a",
+    }
+}
+
 return {
-    'folke/tokyonight.nvim',
+    'EdenEast/nightfox.nvim',
     -- Don't lazy load since this is the main colorscheme
     lazy = false,
     -- Make sure this loads before everything else
     priority = 1000,
     -- Load the colorscheme
     config = function()
-        vim.cmd([[colorscheme tokyonight-moon]])
+        require('nightfox').setup({
+            options = {
+                styles = {
+                    comments = "italic",
+                    functions = "bold",
+                }
+            },
+            palettes = palettes,
+        })
+        vim.cmd([[colorscheme nightfox]])
     end,
-}
-
+} 
