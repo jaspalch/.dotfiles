1802b388312636ec1e7a20989014372c03043482
Jaspal Chauhan <jaspalch95@gmail.com>
2024-07-09 01:25:44 -0700

    Fix prompt spacing

--- roles/bash/files/bashrc_lib/ps1.sh
+++ roles/bash/files/bashrc_lib/ps1.sh
@@ -13,7 +13,7 @@ _GIT() {
     local _git_info
     [[ ! -z "${_git_branch}" ]] && _git_info="${_git_branch}"
     [[ ! -z "${_git_diff}" ]] && _git_info="${_git_info} ${_git_diff}"
-    [[ ! -z "${_git_info}" ]] && echo -n "  [${_git_info}] "
+    [[ ! -z "${_git_info}" ]] && echo -n "  [${_git_info}]"
 }
 
 export -f _GIT
@@ -23,7 +23,7 @@ _PS1() {
 
     _BOLD() {
         local bold="$(tput bold)"
-        echo -n "\001${bold}\002${1}\001${_reset}\002" 2>/dev/null  
+        echo -n "\001${bold}\002${1}\001${_reset}\002" 2>/dev/null
     }
     _COLOR() {
         declare -A colors=(["black"]="0" ["red"]="1" ["green"]="2" ["yellow"]="3" ["blue"]="4" ["purple"]="5" ["cyan"]="6" ["white"]="7")
@@ -31,12 +31,12 @@ _PS1() {
         echo -n "\001${code}\002${2}\001${_reset}\002" 2>/dev/null
     }
 
-    local _user="$(_BOLD $(_COLOR purple $USER ))"
-    local _hostname="$(_BOLD $(_COLOR blue \\h ))"
-    local _pwd="$(_BOLD $(_COLOR cyan [\\w] ))"
+    local _user="$(_BOLD $(_COLOR purple $USER))"
+    local _hostname="$(_BOLD $(_COLOR blue \\h))"
+    local _pwd="$(_BOLD $(_COLOR cyan [\\w]))"
     local _git="$(_BOLD $(_COLOR green '$(_GIT)'))"
 
-    PS1="${_user}$(_COLOR green '@')${_hostname}${_pwd}${_git}$(_BOLD $(_COLOR yellow \$)) "
+    PS1="${_user}$(_COLOR green '@')${_hostname}${_pwd}${_git} $(_BOLD $(_COLOR yellow \$)) "
 }
 
 _PS1
