72f8da26f8049643920adda4594095b8c6e49383
Jaspal Chauhan <jaspalch95@gmail.com>
2025-11-20 18:40:58 -0800

    Switch back to nvim-autopairs, update lazy-lock.json

--- dots/nvim/lua/my/plugins/cmp.lua
+++ dots/nvim/lua/my/plugins/cmp.lua
@@ -6,11 +6,6 @@ return {
 
     -- use a release tag to download pre-built binaries
     version = "1.2.0",
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
-
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
@@ -26,13 +21,13 @@ return {
         -- C-k: Toggle signature help (if signature.enabled = true)
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = "enter" },
+        keymap = {
+            preset = "enter",
+        },
 
         signature = { enabled = true },
 
         appearance = {
-            -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- Adjusts spacing to ensure icons are aligned
             nerd_font_variant = "mono",
         },
 
@@ -45,11 +40,6 @@ return {
             default = { "lsp", "path", "snippets", "buffer" },
         },
 
-        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-        --
-        -- See the fuzzy documentation for more information
         fuzzy = { implementation = "prefer_rust_with_warning" },
     },
     -- opts_extend = { "sources.default" },
